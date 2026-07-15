78aa58b6216c6c638b028ad588b802b7d3502de1
Tenuka22 <tenukaomaljith2009@gmail.com>
2026-07-15 21:15:51 +0530

    fix: cargo fmt formatting issues in counter and mod routes

--- apps/api/rpc/src/routes/root/mod.rs
+++ apps/api/rpc/src/routes/root/mod.rs
@@ -1,10 +1,13 @@
 use actix_web::web;
 
-mod hello;
 mod counter;
+mod hello;
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/hello", web::get().to(hello::hello))
         .route("/counter/{name}", web::get().to(counter::get_counter))
-        .route("/counter/{name}/increment", web::get().to(counter::increment_counter));
+        .route(
+            "/counter/{name}/increment",
+            web::get().to(counter::increment_counter),
+        );
 }
