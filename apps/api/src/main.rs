e10234293b5b091dc57d8d952d7ec35e7297a054
Tenuka22 <tenukaomaljith2009@gmail.com>
2026-07-16 00:44:10 +0530

    style: fix cargo fmt formatting

--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -5,7 +5,9 @@ use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    from_filename(".env.local").or_else(|_| from_filename(".env.production")).ok();
+    from_filename(".env.local")
+        .or_else(|_| from_filename(".env.production"))
+        .ok();
     env_logger::init();
 
     log::info!("Logger initialized");
